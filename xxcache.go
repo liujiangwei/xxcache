e9aa2990770b8082947df791ce949ede7e79430f
liujiangwei <ljw244280@autonavi.com>
2020-06-09 14:59:25 +0800

    go fmt

--- xxcache.go
+++ xxcache.go
@@ -14,30 +14,31 @@ type XXCache interface {
 
 type Option struct {
 	RedisMasterAddr string
-	RedisRdbFile string
-	database int
+	RedisRdbFile    string
+	database        int
 }
 
 type Client struct {
 	option Option
-	redis *redis.Redis
-	cache *cache.Cache
+	redis  *redis.Redis
+	cache  *cache.Cache
 }
 
 const redisMasterAddr = "127.0.0.1:6379"
 const redisRdbFile = "temp.rdb"
-func New(option Option) (client Client, err error){
+
+func New(option Option) (client Client, err error) {
 	client.option = option
 
 	client.cache = new(cache.Cache)
 	prepareOption(&option)
 
 	repl := redis.Replication{
-		MasterAddr:option.RedisMasterAddr,
-		RdbFile:option.RedisRdbFile,
+		MasterAddr: option.RedisMasterAddr,
+		RdbFile:    option.RedisRdbFile,
 	}
 
-	if err = repl.SyncWithRedis(); err != nil{
+	if err = repl.SyncWithRedis(); err != nil {
 		return client, err
 	}
 
@@ -45,49 +46,47 @@ func New(option Option) (client Client, err error){
 
 	go client.handleError(repl)
 
-	if err = repl.Load(); err != nil{
+	if err = repl.Load(); err != nil {
 		return client, err
 	}
 
 	go repl.WaitForMessage()
 
-	client.redis = redis.New(redis.Options{Addr:option.RedisMasterAddr})
+	client.redis = redis.New(redis.Options{Addr: option.RedisMasterAddr})
 
 	return client, err
 }
-
 
 func (client *Client) handleMessage(repl redis.Replication) {
 	for {
 		select {
 		case message := <-repl.Messages:
-			if message.Database != client.option.database{
+			if message.Database != client.option.database {
 				continue
 			}
 
-			if err :=  cache.HandleMessage(client.cache, message.Message); err != nil{
+			if err := cache.HandleMessage(client.cache, message.Message); err != nil {
 				repl.Err <- err
 			}
 		}
 	}
 }
 
-func (client *Client)handleError(repl redis.Replication)  {
+func (client *Client) handleError(repl redis.Replication) {
 	for {
-		err := <- repl.Err
-		if err != nil{
+		err := <-repl.Err
+		if err != nil {
 			logrus.Warnln("repl error", err)
 		}
 	}
 }
 
-
 func prepareOption(option *Option) {
-	if option.RedisMasterAddr == ""{
+	if option.RedisMasterAddr == "" {
 		option.RedisMasterAddr = redisMasterAddr
 	}
 
-	if option.RedisRdbFile == ""{
+	if option.RedisRdbFile == "" {
 		option.RedisRdbFile = redisRdbFile
 	}
 }
